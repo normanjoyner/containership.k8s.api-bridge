7a5f76514cd3e286238cef0bda8c6823f02e0145
Jeremy Kross <jeremy.kross@containership.io>
2017-05-01 15:34:43 +0000

    05/2017 checkpoint.

--- index.js
+++ index.js
@@ -4,27 +4,47 @@ const request = require('request');
 const bodyParser = require('body-parser');
 const prettyjson = require('prettyjson');
 
+const PluginLoader = require('containership.plugin-loader');
 const KubernetesApi = require('./lib/kubernetes-api');
 const routes = require('./lib/routes');
 
 const server = express();
 server.use(bodyParser.json());
 
-const api = new KubernetesApi();
+const mode = process.argv[3] ? process.argv[3] : "leader";
+const ip = process.argv[4] ? process.argv[4] : "0.0.0.0";
+
+const api = new KubernetesApi(ip, 8080); 
 routes.register(server, api);
 
 const testApplication = {
-    id: 'swagger-ui',
-    image: 'schickling/swagger-ui',
-    cpus: 0.25,
+    id: "test",
+    image: 'jeremykross/containership.cloud.loadbalancer:latest',
+    cpus: 0.1,
     memory: 128,
-    container_port: 80,
-    engine: 'docker',
-    command: '',
-    network_mode: 'bridge',
-    privileged: 'false',
-    respawn: 'true'
+    network_mode: 'host',
+    container_port: 3000,
+    host_port: 3001,
+    tags: {
+        constraints: {
+            per_host: 1
+        },
+        foo: "bar"
+    },
+    env_vars: {
+        CS_LEADER_IP: "127.0.0.1",
+        CS_ORCHESTRATOR: "kubernetes"
+    },
+   volumes: [{
+       host: '/var/log/containership',
+       container: '/var/log/containership'
+   }]
 };
+
+const k8sApplication = _.omit(KubernetesApi.csToK8SApp(testApplication), [['spec', 'template']]);
+
+console.log("Conversin: " + JSON.stringify(k8sApplication));
+console.log("And back: " + JSON.stringify(KubernetesApi.k8sToCSApp(k8sApplication.spec.template)));
 
 function doRequest(form) {
     console.log("\n");
@@ -40,13 +60,44 @@ function doRequest(form) {
     });
 }
 
-function startServer() {
-    const port = 9443;
-    server.listen(port);
-    console.log(`Listening on: ${port}`);
+function installRoutes(server, namespace, routes) {
+    _.each(routes, (route) => {
+        console.log("Checking route: " + JSON.stringify(route));
+        if(route.method === "GET") {
+            console.log(`Installing route ${namespace}${route.path} with handler ${route.handler}`);
+            server.get(`/:apiVersion/${namespace}${route.path}`, route.handler);
+        } else if (route.method === "POST") {
+            server.post(`/:apiVersion/${namespace}${route.path}`, route.handler);
+        }
+    });
 }
 
-if(process.argv[2] === 'start') {
+function startServer() {
+
+    const host = new PluginLoader.K8SHost(mode, "localhost", "9443");;
+
+    PluginLoader.load((plugins) => {
+
+        _.each(plugins, (Plugin) => {
+            try {
+                const p = new Plugin();
+                const routes = p.getApiRoutes(host);
+
+                console.log("Installing routes for: " + p.name + " " + JSON.stringify(routes));
+
+                installRoutes(server, p.name, routes);
+            } catch(err) {
+                console.log("Error installing routes for plugin: " + err);
+            }
+        });
+
+        const port = 9443;
+        server.listen(port);
+        console.log(`Listening on: ${port}`);
+    });
+}
+
+if(require.main === module && process.argv[2] === 'start') {
     startServer();
 }
 
